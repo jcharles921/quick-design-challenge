daa34893cb0502a64df845fe832f7a8622df34cf
jcharles921 <babouwitonze921@gmail.com>
2024-06-07 18:10:48 +0200

    applying styles

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello Challenge")
+document.addEventListener("DOMContentLoaded", function() {
+    const imgContainers = document.querySelectorAll('.imgContainer');
+    const images = [
+        "./assets/Fox.jpg",
+        "./assets/Whale.jpg",
+        "./assets/Baboon.jpg",
+        "./assets/Deer.jpg"
+    ];
+
+    imgContainers.forEach((container, index) => {
+        container.style.backgroundImage = `url('${images[index]}')`;
+        container.style.backgroundSize = "cover";
+        container.style.backgroundPosition = "center";
+        container.style.backgroundRepeat = "no-repeat";
+    });
+});
